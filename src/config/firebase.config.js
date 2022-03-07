0f4aad6f01f6dfc8e306ba68c66894ec05edf9fe
Bartek Bronkowski <84997081+pchelka90@users.noreply.github.com>
2022-03-07 17:21:14 +0100

    Add files via upload

---
+++ src/config/firebase.config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBL-acinpDhBOOBa8fYumq9CHVJcydoz68',
+  authDomain: 'recipereactapp-9eeff.firebaseapp.com',
+  databaseURL:
+    'https://recipereactapp-9eeff-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'recipereactapp-9eeff',
+  storageBucket: 'recipereactapp-9eeff.appspot.com',
+  messagingSenderId: '1093781272307',
+  appId: '1:1093781272307:web:c97035657621c95e4e2986',
+};
